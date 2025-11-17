d27d8f7fed389f03340531336312df66bda45e06
DuhaRabaya <duharabaya4@gmail.com>
2025-11-17 23:12:07 +0200

    first commit

---
+++ DSHOP.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DSHOP.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ DSHOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DSHOP.DAL.Data;
+
+namespace DSHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
